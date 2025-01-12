3feccebeecc33ba3cbc15ca8c00aa3257409d478
Whois Arvian <arviannewbie473@gmail.com>
2025-01-13 02:55:25 +0700

    update

--- index.js
+++ index.js
@@ -186,6 +186,8 @@ async function register(email, password, username) {
 
             const response = await axios.post('https://gw.sosovalue.com/usercenter/email/anno/sendRegisterVerifyCode/V2', data, axiosConfig);
             console.log(chalk.green(`[+] Registration successful for ${email}`));
+            console.log(chalk.green(`[+] Registration successful for ${response.data}`));
+            console.log(chalk.green(`[+] Registration successful for ${response.code}`));
             return response.data;
         } catch (error) {
             console.log(chalk.red(`[!] Registration failed: ${error.message}`));
